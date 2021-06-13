47182cd47dfd1986c58169379906a542bc69d74c
igar15 <igar15@yandex.ru>
2021-06-13 21:56:44 +0300

    Add AppExceptionHandler

--- src/main/java/ru/javaprojects/albumaccounting/util/ValidationUtil.java
+++ src/main/java/ru/javaprojects/albumaccounting/util/ValidationUtil.java
@@ -1,9 +1,13 @@
 package ru.javaprojects.albumaccounting.util;
 
+import org.slf4j.Logger;
 import org.springframework.core.NestedExceptionUtils;
 import org.springframework.lang.NonNull;
 import ru.javaprojects.albumaccounting.HasId;
+import ru.javaprojects.albumaccounting.util.exception.ErrorType;
 import ru.javaprojects.albumaccounting.util.exception.IllegalRequestDataException;
+
+import javax.servlet.http.HttpServletRequest;
 
 public class ValidationUtil {
 
@@ -25,10 +29,24 @@ public class ValidationUtil {
         }
     }
 
+    public static String getMessage(Throwable e) {
+        return e.getMessage() != null ? e.getMessage() : e.getClass().getName();
+    }
+
     //  https://stackoverflow.com/a/65442410/548473
     @NonNull
     public static Throwable getRootCause(@NonNull Throwable t) {
         Throwable rootCause = NestedExceptionUtils.getRootCause(t);
         return rootCause != null ? rootCause : t;
+    }
+
+    public static Throwable logAndGetRootCause(Logger log, HttpServletRequest req, Exception e, boolean logStackTrace, ErrorType errorType) {
+        Throwable rootCause = getRootCause(e);
+        if (logStackTrace) {
+            log.error(errorType + " at request " + req.getRequestURL(), rootCause);
+        } else {
+            log.warn("{} at request  {}: {}", errorType, req.getRequestURL(), rootCause.toString());
+        }
+        return rootCause;
     }
 }

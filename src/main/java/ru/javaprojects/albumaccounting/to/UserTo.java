90b1bbc79cf3f1c9911089b030c35ae3edcddf30
igar15 <igar15@yandex.ru>
2021-06-13 19:32:37 +0300

    Add UserTo

---
+++ src/main/java/ru/javaprojects/albumaccounting/to/UserTo.java
@@ -0,0 +1,86 @@
+package ru.javaprojects.albumaccounting.to;
+
+import ru.javaprojects.albumaccounting.HasId;
+import ru.javaprojects.albumaccounting.model.Role;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Set;
+
+public class UserTo implements HasId, Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String name;
+
+    @Email
+    @NotBlank
+    @Size(max = 100)
+    private String email;
+
+
+    @NotEmpty
+    private Set<Role> roles;
+
+    public UserTo() {
+    }
+
+    public UserTo(Integer id, String name, String email, Set<Role> roles) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "UserTo{" +
+                "id=" + id +
+                ", name=" + name +
+                ", email=" + email +
+                ", roles=" + roles +
+                '}';
+    }
+}
